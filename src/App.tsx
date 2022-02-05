2d340fb677a225396231487918322ae544eeb4bd
Sushant <sushant1999official@gmail.com>
2022-02-05 11:36:34 +0530

    Added countries to be fetched from disease.sh API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import { FormControl, Select, MenuItem } from "@mui/material";
 import React from "react";
+import { map } from "lodash";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = React.useState<any>([]);
+
+  React.useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = map(data, (country: any) => {
+            return {
+              name: country.country,
+              value: country.countryInfo.iso2,
+            };
+          });
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, [countries]);
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {map(countries, (country: any, index) => {
+              return (
+                <MenuItem value={country.value} key={index}>
+                  {country.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
